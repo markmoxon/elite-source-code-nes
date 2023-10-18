0e406943fa77b6e32184b1599479102e67262638
Mark Moxon <mark@moxon.net>
2023-10-18 16:53:26 +0100

    Added colour details to the hyperspace colour table

--- 1-source-files/main-sources/elite-source-bank-6.asm
+++ 1-source-files/main-sources/elite-source-bank-6.asm
@@ -13576,8 +13576,22 @@ ENDIF
 
 .hyperspaceColour
 
- EQUB $06, $0F, $38, $2A, $23, $25, $22, $11
- EQUB $1A, $00, $26, $2C, $20, $13, $0F, $00
+ EQUB $06               ; Dark red
+ EQUB $0F               ; Black
+ EQUB $38               ; Light brown
+ EQUB $2A               ; Medium green
+ EQUB $23               ; Medium purple
+ EQUB $25               ; Medium magenta
+ EQUB $22               ; Medium blue
+ EQUB $11               ; Medium-dark blue
+ EQUB $1A               ; Medium-dark green
+ EQUB $00               ; Dark grey
+ EQUB $26               ; Light orange
+ EQUB $2C               ; Light cyan
+ EQUB $20               ; White
+ EQUB $13               ; Medium-dark 
+ EQUB $0F               ; Black
+ EQUB $00               ; Dark grey
 
 ; ******************************************************************************
 ;
