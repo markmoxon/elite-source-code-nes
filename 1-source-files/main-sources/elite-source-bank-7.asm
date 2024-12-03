2dd90b4e384f59bbf5779c6e9f03fd38eacf9210
Mark Moxon <mark@moxon.net>
2024-12-03 16:03:50 +0000

    Add "BBC Micro" to mentions of the cassette and disc versions, to be clear

--- 1-source-files/main-sources/elite-source-bank-7.asm
+++ 1-source-files/main-sources/elite-source-bank-7.asm
@@ -17811,8 +17811,10 @@ ENDIF
 
  TAX                    ; Copy A into X. There is a comment in the original
                         ; source here that says "just in case", which refers to
-                        ; the MU11 routine in the cassette and disc versions,
-                        ; which set X to 0 (as they use X as a loop counter).
+                        ; the MU11 routine in the BBC Micro cassette and disc
+                        ; versions, which set X to 0 (as they use X as a loop
+                        ; counter)
+                        ;
                         ; The version here doesn't use a loop, but this
                         ; instruction makes sure the unrolled version returns
                         ; the same results as the loop versions, just in case
@@ -17822,12 +17824,12 @@ ENDIF
                         ; and C flag = bit 0 of P
 
                         ; We now repeat the following four instruction block
-                        ; eight times, one for each bit in P. In the cassette
-                        ; and disc versions of Elite the following is done with
-                        ; a loop, but it is marginally faster to unroll the loop
-                        ; and have eight copies of the code, though it does take
-                        ; up a bit more memory (though that isn't a concern when
-                        ; you have a 6502 Second Processor)
+                        ; eight times, one for each bit in P. In the BBC Micro
+                        ; cassette and disc versions of Elite the following is
+                        ; done with a loop, but it is marginally faster to
+                        ; unroll the loop and have eight copies of the code,
+                        ; though it does take up a bit more memory (though that
+                        ; isn't a concern when you have a 6502 Second Processor)
 
  BCC P%+4               ; If C (i.e. bit 0 of P) is set, do the
  ADC T                  ; addition for this bit of P:
@@ -18782,12 +18784,13 @@ ENDIF
  LDA #0                 ; Set A = 0 for us to build a remainder
 
                         ; We now repeat the following five instruction block
-                        ; eight times, one for each bit in P. In the cassette
-                        ; and disc versions of Elite the following is done with
-                        ; a loop, but it is marginally faster to unroll the loop
-                        ; and have eight copies of the code, though it does take
-                        ; up a bit more memory (though that isn't a concern when
-                        ; you have a 6502 Second Processor)
+                        ; eight times, one for each bit in P. In the BBC Micro
+                        ; cassette and disc versions of Elite the following is
+                        ; done with a loop, but it is marginally faster to
+                        ; unroll the loop and have eight copies of the code,
+                        ; though it does take up a bit more memory (though that
+                        ; isn't a big concern when you have a 6502 Second
+                        ; Processor)
 
  ROL A                  ; Shift A to the left
 
