fca8acf97ba8fd7a269e61df6deee9a1162730c2
Mark Moxon <mark@moxon.net>
2025-01-30 10:21:51 +0000

    Rename brace-fix labels to use the standard 'K' suffix

--- 1-source-files/main-sources/elite-source-bank-2.asm
+++ 1-source-files/main-sources/elite-source-bank-2.asm
@@ -16855,22 +16855,22 @@ ENDIF
                         ; every time we call PDESC, so set a counter in X for
                         ; copying 4 bytes
 
-{
-.PDL1                   ; This label is a duplicate of the label above (which is
-                        ; why we need to surround it with braces, as BeebAsm
-                        ; doesn't allow us to redefine labels, unlike BBC BASIC)
+.PDL1K                  ; This label is a duplicate of the label above
+                        ;
+                        ; In the original source this label is PDL1, but
+                        ; because BeebAsm doesn't allow us to redefine labels,
+                        ; I have renamed it to PDL1K
 
  LDA QQ15+2,X           ; Copy QQ15+2 to QQ15+5 (s1 and s2) to RAND to RAND+3
  STA RAND,X
 
  DEX                    ; Decrement the loop counter
 
- BPL PDL1               ; Loop back to PDL1 until we have copied all
+ BPL PDL1K              ; Loop back to PDL1K until we have copied all
 
  LDA #5                 ; Set A = 5, so we print extended token 5 in the next
                         ; instruction ("{lower case}{justify}{single cap}[86-90]
                         ; IS [140-144].{cr}{left align}"
-}
 
 .PD4
 
