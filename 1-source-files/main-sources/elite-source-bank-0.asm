773c5ad19911e0729b673c228b58e03ba1f8ed1b
Mark Moxon <mark@moxon.net>
2025-02-17 11:47:50 +0000

    Minor improvements to sound effect commentary

--- 1-source-files/main-sources/elite-source-bank-0.asm
+++ 1-source-files/main-sources/elite-source-bank-0.asm
@@ -24078,7 +24078,7 @@ ENDIF
 ;       Name: EXNO
 ;       Type: Subroutine
 ;   Category: Sound
-;    Summary: Make the sound of a laser strike or ship explosion
+;    Summary: Make the sound of a laser strike on another ship
 ;
 ; ------------------------------------------------------------------------------
 ;
