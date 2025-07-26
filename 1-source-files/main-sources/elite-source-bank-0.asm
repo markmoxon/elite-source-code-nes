bd88e5030f2c80befe853913327ba0af6b4033bb
Mark Moxon <mark@moxon.net>
2025-07-26 15:02:58 +0100

    Improved commentary around the cabin temperature calculations

--- 1-source-files/main-sources/elite-source-bank-0.asm
+++ 1-source-files/main-sources/elite-source-bank-0.asm
@@ -1789,25 +1789,39 @@ ENDIF
                         ; jump to MA23 to skip the following, as we are too far
                         ; from the sun for scooping or temperature changes
 
- JSR MAS3               ; Set A = x_hi^2 + y_hi^2 + z_hi^2, so using Pythagoras
-                        ; we now know that A now contains the square of the
-                        ; distance between our ship (at the origin) and the
-                        ; heart of the sun at (x_hi, y_hi, z_hi)
+ JSR MAS3               ; Set (A ?) = x_hi^2 + y_hi^2 + z_hi^2, so using
+                        ; Pythagoras we now know that A now contains the high
+                        ; byte of the square of the distance between our ship
+                        ; (at the origin) and the heart of the sun at coordinate
+                        ; (x_hi, y_hi, z_hi)
+                        ;
+                        ; If the calculation overflows so it doesn't fit into
+                        ; one byte, then A is set to $FF and the C flag is set
 
  EOR #%11111111         ; Invert A, so A is now small if we are far from the
                         ; sun and large if we are close to the sun, in the
                         ; range 0 = far away to $FF = extremely close, ouch,
                         ; hot, hot, hot!
 
- ADC #30                ; Add the minimum cabin temperature of 30, so we get
-                        ; one of the following:
+ ADC #30                ; Add the minimum cabin temperature of 30, plus the C
+                        ; flag, so we get one of the following:
                         ;
-                        ;   * If the C flag is clear, A contains the cabin
-                        ;     temperature, ranging from 30 to 255, that's hotter
-                        ;     the closer we are to the sun
                         ;
-                        ;   * If the C flag is set, the addition has rolled over
-                        ;     and the cabin temperature is over 255
+                        ;   * If the MAS3 calculation overflowed then we are a
+                        ;     long way from the sun, A will be zero and the C
+                        ;     flag will be set, so this addition sets A = 31
+                        ;     and clears the C flag
+                        ;
+                        ;   * If the result of the MAS3 calculation fitted into
+                        ;     one byte, then A will be in the range 0 to 255 and
+                        ;     the C flag will be clear, so this addition has a
+                        ;     result in the range 0 to 285, with the higher
+                        ;     values overflowing the addition and setting the
+                        ;     C flag
+                        ;
+                        ; So the C flag is set if the cabin temperature is too
+                        ; hot to handle, and if it's clear then A contains the
+                        ; cabin temperature
 
  STA CABTMP             ; Store the updated cabin temperature
 
@@ -1983,7 +1997,10 @@ ENDIF
                         ; fair distance from the planet, so jump to MA232 as we
                         ; haven't crashed into the planet
 
- SBC #36                ; Subtract 36 from x_hi^2 + y_hi^2 + z_hi^2
+ SBC #36                ; Subtract 37 from x_hi^2 + y_hi^2 + z_hi^2
+                        ;
+                        ; The SBC subtracts 37 as we just passed through a BCS
+                        ; so we know the C flag is clear
                         ;
                         ; When we do the 3D Pythagoras calculation, we only use
                         ; the high bytes of the coordinates, so that's x_hi,
@@ -2002,7 +2019,9 @@ ENDIF
                         ; So for the planet, the equivalent figure to test the
                         ; sum of the _hi bytes against is 36, so A now contains
                         ; the high byte of our altitude above the planet
-                        ; surface, squared
+                        ; surface, squared, with an extra 1 subtracted so the
+                        ; test in the next instruction will ensure we crash
+                        ; even if we are exactly one planet radius away
 
  BCC MA282              ; If A < 0 then jump to MA282 as we have crashed into
                         ; the planet
@@ -20410,7 +20429,7 @@ ENDIF
 ; Given a value in Y that points to the start of a ship data block as an offset
 ; from K%, calculate the following:
 ;
-;   A = x_hi^2 + y_hi^2 + z_hi^2
+;   (A ?) = x_hi^2 + y_hi^2 + z_hi^2
 ;
 ; returning A = $FF if the calculation overflows a one-byte result. The K%
 ; workspace contains the ship data blocks, so the offset in Y must be 0 or a
@@ -20425,9 +20444,15 @@ ENDIF
 ;
 ; Returns
 ;
-;   A                   A = x_hi^2 + y_hi^2 + z_hi^2
+;   A                   The high byte of x_hi^2 + y_hi^2 + z_hi^2
 ;
-;                       A = $FF if the calculation overflows a one-byte result
+;   C flag              The overflow status (i.e. did the result fit into one
+;                       byte):
+;
+;                         * Clear if the calculation didn't overflow
+;
+;                         * Set if the calculation overflowed (in which case A
+;                           is set to $FF)
 ;
 ; ******************************************************************************
 
@@ -20455,7 +20480,8 @@ ENDIF
  JSR SQUA2
 
  ADC R                  ; Add A (high byte of third result) to R, so R now
-                        ; contains the sum of x_hi^2 + y_hi^2 + z_hi^2
+                        ; contains the high byte of the entire sum, i.e. of
+                        ; x_hi^2 + y_hi^2 + z_hi^2
 
  BCC P%+4               ; If there is no carry, skip the following instruction
                         ; to return straight from the subroutine
