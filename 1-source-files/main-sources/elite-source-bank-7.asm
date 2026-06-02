6d54fe93a2d800d67bd4e013885a1eff36f27eed
Mark Moxon <mark@moxon.net>
2026-06-02 12:20:49 +0100

    Rename line-drawing deep dive

--- 1-source-files/main-sources/elite-source-bank-7.asm
+++ 1-source-files/main-sources/elite-source-bank-7.asm
@@ -6352,7 +6352,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a line: Calculate the line gradient in the form of deltas
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
@@ -6437,7 +6437,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a line: Line has a shallow gradient, step right along x-axis
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
@@ -6580,7 +6580,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a shallow line going right and up or left and down
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
@@ -6797,7 +6797,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a shallow line going right and down or left and up
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
@@ -7027,7 +7027,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a line: Line has a steep gradient, step up along y-axis
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
@@ -7203,7 +7203,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a steep line going up and left or down and right
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
@@ -7524,7 +7524,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a steep line going up and right or down and left
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;             Drawing lines in the NES version
 ;
 ; ------------------------------------------------------------------------------
