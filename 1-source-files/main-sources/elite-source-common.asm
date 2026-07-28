9866e5ebf52fda9b2c4dcb849c93981e7b29dbb8
Mark Moxon <mark@moxon.net>
2026-07-28 23:16:34 +0100

    Fix incorrect comments on LASCT decrement

--- 1-source-files/main-sources/elite-source-common.asm
+++ 1-source-files/main-sources/elite-source-common.asm
@@ -3252,14 +3252,14 @@
                         ;
                         ;   * 10 for a pulse laser
                         ;
-                        ; It gets decremented by 2 on each iteration round the
+                        ; It gets decremented by 2 on each iteration around the
                         ; main game loop and is set to a non-zero value for
                         ; pulse lasers only
                         ;
                         ; The laser only fires when the value of LASCT hits
                         ; zero, so for pulse lasers with a value of 10, that
-                        ; means the laser fires once every four iterations
-                        ; round the main game loop (LASCT = 10, 6, 2, 0)
+                        ; means the laser fires once every five iterations
+                        ; around the main game loop (LASCT = 10, 8, 6, 4, 2, 0)
                         ;
                         ; In comparison, beam lasers fire continuously as the
                         ; value of LASCT is always 0
