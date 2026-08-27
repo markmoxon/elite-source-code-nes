ccf73a76abae9f0cd7b2955bc172882ae2e78bb0
Mark Moxon <mark@moxon.net>
2026-08-27 17:01:12 +0100

    Correction to sign of y-coordinate in comments in stardust drawing routine

--- 1-source-files/main-sources/elite-source-bank-1.asm
+++ 1-source-files/main-sources/elite-source-bank-1.asm
@@ -14615,8 +14615,10 @@ ENDIF
                         ; to skip the following negation
 
  EOR #%01111111         ; The y-coordinate offset is negative, so flip all the
- ADC #1                 ; bits apart from the sign bit and add 1, to convert it
-                        ; from a sign-magnitude number to a signed number
+ ADC #1                 ; bits apart from the sign bit and subtract 1 to convert
+                        ; A from a sign-magnitude number into a traditional
+                        ; signed number, so A is now Y1 in a form that can be
+                        ; used with the SBC instruction
 
 .PX22
 
